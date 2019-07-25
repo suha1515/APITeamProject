3945b466f83908a8513619315c13b1647f4db220
hwiimoon <hwiimoon@naver.com>
2019-07-25 15:27:11 +0900

    Revert "update from master"
    This reverts commit d8ab608376671d9b09323f1f93813b1b7ca0d632, reversing
    changes made to 7178d4446fb5cdb97a6d1585c130d8c9888a823c.

--- Framework99C/CollsionMgr.h
+++ Framework99C/CollsionMgr.h
@@ -1,21 +1,18 @@
 #pragma once
 class CCollsionMgr
 {
-DECLARE_SINGLE_TONE(CCollsionMgr)
-
-private:
-	void Initialize();
+public:
+	CCollsionMgr();
+	~CCollsionMgr();
 
 public:
 	// 단순충돌용 AABB
-	bool CollisionRect(const OBJLIST& dstLst, const OBJLIST& srcLst);
+	static bool CollisionRect(const OBJLIST& dstLst, const OBJLIST& srcLst);
 	// 충돌범위를 VECTOR로 반환하는 AABB 버전 3번째 인자에 주소로 넘기면된다.
-
-	bool CollisionRect(const OBJLIST& dstLst, const OBJLIST& srcLst,VECTOR2D* depth);
-	void CollisionSphere(const OBJLIST& dstLst, const OBJLIST& srcLst);
-
-	bool CollisionRectEX(const OBJLIST& dstLst, const OBJLIST& srcLst);
+	static bool CollisionRect(const OBJLIST& dstLst, const OBJLIST& srcLst,VECTOR2D* depth);
+	static void CollisionSphere(const OBJLIST& dstLst, const OBJLIST& srcLst);
 
 private:
-	bool CheckSphere(const CGameObject* pDest, const CGameObject* pSource);
+	static bool CheckSphere(const CGameObject* pDest, const CGameObject* pSource);
 };
+
